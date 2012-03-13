53732e39defec355e1f63253bc7df4f023ecb6d4
Meldanor <Bhaals@gmx.de>
2012-03-13 22:01:16 +0100

    Update licence and add missing copyrights
    Signed-off-by: Meldanor <Bhaals@gmx.de>

--- src/main/java/de/minestar/contao2/manager/PlayerManager.java
+++ src/main/java/de/minestar/contao2/manager/PlayerManager.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2012 MineStar.de 
+ * 
+ * This file is part of Contao2.
+ * 
+ * Contao2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * Contao2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Contao2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.minestar.contao2.manager;
 
 import java.io.BufferedWriter;
