68e39fbe884fd2a97309c2aae26b50a137ae85b7
GeMoschen <x-chris-x@arcor.de>
2013-05-09 11:55:02 +0200

    Revert "- do not save statistics on disable"
    This reverts commit 330a6efdb41fb863ccc3630ea872b25e09311ca8.

--- src/main/java/de/minestar/contao2/core/Core.java
+++ src/main/java/de/minestar/contao2/core/Core.java
@@ -171,6 +171,7 @@ public class Core extends AbstractCore {
     @Override
     protected boolean commonDisable() {
         if (databaseManager != null) {
+            statisticManager.saveAllStatistics();
             databaseManager.closeConnection();
         }
 
